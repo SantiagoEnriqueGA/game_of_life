792e0c678445799f385f97468b590923ddcefd7f
SantiagoEnriqueGA <sega97@gmail.com>
2024-04-28 12:32:06 -0500

    added comments

--- gameOfLife.py
+++ gameOfLife.py
@@ -6,45 +6,54 @@ import numpy as np
 
 pygame.font.init()
 
+# Define colors for the cells and background
 col_about_to_die = (200, 200, 225)
 col_about_to_die = (255, 0, 0) #red
 col_alive = (255, 255, 215)
 col_background = (10, 10, 40)
 col_grid = (30, 30, 60)
 
+# Initialize a font for displaying generation information
 myfont = pygame.font.SysFont("monospace", 16)
 
-
+# Function to update the game state and draw cells
 def update(surface, cur, sz, gen):
     nxt = np.zeros((cur.shape[0], cur.shape[1]))
-    
+
+    # Iterate over each cell in the current state
     for r, c in np.ndindex(cur.shape):
+        # Count the number of alive neighbors for each cell
         num_alive = np.sum(cur[r-1:r+2, c-1:c+2]) - cur[r, c]
 
+        # Update cell colors based on Conway's Game of Life rules
         if cur[r, c] == 1 and num_alive < 2 or num_alive > 3:
             col = col_about_to_die
         elif (cur[r, c] == 1 and 2 <= num_alive <= 3) or (cur[r, c] == 0 and num_alive == 3):
             nxt[r, c] = 1
             col = col_alive
 
+        # Draw cells on the surface with appropriate colors
         col = col if cur[r, c] == 1 else col_background
         pygame.draw.rect(surface, col, (c*sz, r*sz, sz-1, sz-1))
          
+    # Display the current generation information on the surface
     gentext = myfont.render("Generation: {0}".format(gen), 1, (255, 255, 255))
     surface.blit(gentext, (0, len(cur)*sz-16))        
             
     return nxt
 
-
+# Initialize the game grid with a given x and y size
 def init(dimx, dimy, pattern):
     cells = np.zeros((dimy, dimx))
     cells = pattern
     
     return cells
 
+# Initialize the game grid with gliders pattern
 def init_gliders(dimx, dimy, pattern):
     cells = np.zeros((dimy, dimx))
 
+    # Define patterns for one or two Gosper's glider guns
     if('1' in pattern):
         # A single Gosper's glider gun creating gliders   
         pattern = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -55,8 +64,7 @@ def init_gliders(dimx, dimy, pattern):
                             [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                             [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                             [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]);
-    
+                            [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
     if('2' in pattern):
         # Two Gosper's glider gun creating gliders   
         pattern = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -67,20 +75,20 @@ def init_gliders(dimx, dimy, pattern):
                             [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,1],
                             [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
                             [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0]]);
+                            [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0]])
 
-    
+    # Position the pattern in the game grid
     pos = (3,3)
     cells[pos[0]:pos[0]+pattern.shape[0], pos[1]:pos[1]+pattern.shape[1]] = pattern
     return cells
 
-
+# Main function to run the game
 def main(dimx, dimy, cellsize, pattern):
     pygame.init()
     surface = pygame.display.set_mode((dimx * cellsize, dimy * cellsize))
     pygame.display.set_caption("Py Game of Life")
     
-#     print(pattern)
+    # Initialize the game grid based on user input
     if('yes' in pattern):
         cells = init_gliders(dimx, dimy, pattern)
     else:
@@ -95,16 +103,19 @@ def main(dimx, dimy, cellsize, pattern):
                 exit()
                 return
         
+        # Fill the surface with the grid color
         surface.fill(col_grid)
+        # Update the game state and draw cells
         cells = update(surface, cells, cellsize, gen)
         pygame.display.update()
         gen+= 1
  
 
-gliders = input("Run a Gosper's glider gun creating gliders? (Yes, No): ").lower()
+# Get user input to determine the game setup
+gliders = input("Run a Gosper's Glider Gun, creating gliders? (Yes, No): ").lower()
 if(gliders == 'yes'):
     gliders += input("1 or 2 Gliders? (1,2): ")
-if(gliders == 'No' or gliders == 'no'):
+if(gliders == 'no'):
     n= int(input("Enter world width: "))
     m= int(input("Enter world height: "))
     s= int(input("Enter cell size: "))
